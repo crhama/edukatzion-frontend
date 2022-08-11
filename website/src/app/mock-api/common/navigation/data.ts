a1a6500c6fd5756c9c5c3c788b59ae28210318e2
crhama <crhamazany@outlook.com>
2022-08-11 19:40:58 -0400

    initial

---
+++ website/src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,55 @@
+/* tslint:disable:max-line-length */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id      : 'students',
+        title   : 'students',
+        subtitle: 'Student Management',
+        type    : 'group',
+        icon    : 'heroicons_outline:home',
+        children: [
+            {
+                id   : 'students.list',
+                title: 'List',
+                type : 'basic',
+                icon : 'heroicons_outline:clipboard-check',
+                link : '/students/list'
+            }
+        ]
+    },
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id   : 'example',
+        title: 'Example',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    }
+];
